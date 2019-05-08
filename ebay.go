f51c4f07c48ca3e11d9889dbae0e7b5d87a68e75
Matej Puntar <matej.puntar@olamobile.om>
2019-05-08 14:09:59 +0200

    new function FindCompletedItemsByKeywords

--- ebay.go
+++ ebay.go
@@ -62,14 +62,14 @@ func New(application_id string) *EBay {
 	return &e
 } 
 
-func (e *EBay) build_search_url(global_id string, keywords string, entries_per_page int) (string, error) {
+func (e *EBay) build_search_url(global_id string, operation_call string, keywords string, entries_per_page int) (string, error) {
 	var u *url.URL
 	u, err := url.Parse("http://svcs.ebay.com/services/search/FindingService/v1")
 	if err != nil {
 		return "", err
 	}
 	params := url.Values{}
-	params.Add("OPERATION-NAME", "findItemsByKeywords")
+	params.Add("OPERATION-NAME", operation_call)
 	params.Add("SERVICE-VERSION", "1.0.0")
 	params.Add("SECURITY-APPNAME", e.ApplicationId)
 	params.Add("GLOBAL-ID", global_id)
@@ -77,6 +77,8 @@ func (e *EBay) build_search_url(global_id string, keywords string, entries_per_p
 	params.Add("REST-PAYLOAD", "")
 	params.Add("keywords", keywords)
 	params.Add("paginationInput.entriesPerPage", strconv.Itoa(entries_per_page))
+	// params.Add("outputSelector(0)", "AspectHistogram")
+	// params.Add("outputSelector(1)", "CategoryHistogram")
 	params.Add("itemFilter(0).name", "ListingType")
 	params.Add("itemFilter(0).value(0)", "FixedPrice")
 	params.Add("itemFilter(0).value(1)", "AuctionWithBIN")
@@ -86,7 +88,7 @@ func (e *EBay) build_search_url(global_id string, keywords string, entries_per_p
 
 func (e *EBay) FindItemsByKeywords(global_id string, keywords string, entries_per_page int) (FindItemsByKeywordResponse, error) {
 	var response FindItemsByKeywordResponse
-	url, err := e.build_search_url(global_id, keywords, entries_per_page)
+	url, err := e.build_search_url(global_id, "findItemsByKeywords", keywords, entries_per_page)
 	if err != nil {
 		return response, err
 	}
@@ -129,4 +131,33 @@ func (r *FindItemsByKeywordResponse) Dump() {
 		fmt.Println("\tSeller Location: ", i.Location)
 		fmt.Println()
 	}
+}
+
+
+func (e *EBay) FindCompletedItemsByKeywords(global_id string, keywords string, entries_per_page int) (FindItemsByKeywordResponse, error) {
+	var response FindItemsByKeywordResponse
+	url, err := e.build_search_url(global_id, "findCompletedItems", keywords, entries_per_page)
+	if err != nil {
+		return response, err
+	}
+	headers := make(map[string]string)
+	headers["User-Agent"] = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11"
+	body, status_code, err := e.HttpRequest.Get(url, headers)
+	if err != nil {
+		return response, err
+	}
+	if status_code != 200 {
+		var em ErrorMessage
+		err = xml.Unmarshal([]byte(body), &em)
+		if err != nil {
+			return response, err
+		}
+		return response, errors.New(em.Error.Message)
+	} else {
+		err = xml.Unmarshal([]byte(body), &response)
+		if err != nil {
+			return response, err
+		}
+	}
+	return response, err
 }
--- ebay_test.go
+++ ebay_test.go
@@ -12,9 +12,34 @@ var (
 func TestFindItemsByKeywords(t *testing.T) {
 	fmt.Println("ebay.FindItemsByKeywords")
 	e := New(test_application_id)
-	response, err := e.FindItemsByKeywords(GLOBAL_ID_EBAY_US, "DJM 900, DJM 850", 10)
+	response, err := e.FindItemsByKeywords(GLOBAL_ID_EBAY_DE, "ORIS DIVERS SIXTY-FIVE", 2)
 	if err != nil {
-		t.Errorf("ERROR: ", err)
+		t.Errorf("ERROR: %v", err)
+	} else {
+		fmt.Println("Timestamp: ", response.Timestamp)
+		fmt.Println("Items:")
+		fmt.Println("------")
+		for _, i := range response.Items {
+			fmt.Println("Title: ", i.Title)
+			fmt.Println("------")
+			fmt.Println("\tListing Url:     ", i.ListingUrl)
+			fmt.Println("\tBin Price:       ", i.BinPrice)
+			fmt.Println("\tCurrent Price:   ", i.CurrentPrice)
+			fmt.Println("\tShipping Price:  ", i.ShippingPrice)
+			fmt.Println("\tShips To:        ", i.ShipsTo)
+			fmt.Println("\tSeller Location: ", i.Location)
+			fmt.Println()
+
+		}
+	}
+}
+
+func TestFindCompletedItemsByKeywords(t *testing.T) {
+	fmt.Println("ebay.FindCompletedItemsByKeywords")
+	e := New(test_application_id)
+	response, err := e.FindCompletedItemsByKeywords(GLOBAL_ID_EBAY_DE, "ORIS DIVERS SIXTY-FIVE", 2)
+	if err != nil {
+		t.Errorf("ERROR: %v", err)
 	} else {
 		fmt.Println("Timestamp: ", response.Timestamp)
 		fmt.Println("Items:")
